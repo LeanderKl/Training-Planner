1fd47d4388962f35f15a7e38dfac15d9a0b92670
LeanderKl <90198306+LeanderKl@users.noreply.github.com>
2022-01-14 14:06:01 +0100

    Initial Commit.

---
+++ src/main/java/dev/wcs/sea/trainingplanner/TrainingPlannerApplication.java
@@ -0,0 +1,13 @@
+package dev.wcs.sea.trainingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wcs/sea/trainingplanner/TrainingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wcs.sea.trainingplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
